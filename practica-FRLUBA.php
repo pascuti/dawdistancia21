4ca1c7ed2eec6ae17e9d787565f15d71f195ddc1
pascuti <pascuti@gmail.com>
2021-04-25 18:58:18 +0200

    Add files via upload
    Fichero principal del repositorio

---
+++ practica-FRLUBA.php
@@ -0,0 +1,51 @@
+<?php
+
+/**
+* Verifica si el formato de una direccion de correo es correcta.
+*
+* Esta funcion comprueba que la direccion de correo porporcionada
+* cumple con la estructura xxxxxx@xxxxxxxx.xx
+* Devuelve true si la estructura es correcta
+*
+* @author Francisco Jose Luna Barsotti
+* @version 1.0.0
+* @param string $email direccion de correo
+* @return boolean true si la direccion es correcta
+*/
+function verifica_email ($email)
+{
+	$retorno = true;
+
+	/* Aqui estaria el codigo para verificar 
+	la direccion de correo */
+
+	return $retorno;
+}
+
+
+/**
+* Genera el token para operaciones permitidas.
+*
+* Esta funcion a partir del usuario logado y el perfil
+* seleccionado, genera un token para poder realizar
+* las operaciones donde el usuario tiene permisos.
+* Devuelve el token asociado.
+*
+* @internal
+* @author Francisco Jose Luna Barsotti
+* @version 2.1.1
+* @param string $user usuario logado en el sistema
+* @param string $profile perfil seleccionado
+* @return string token
+*/
+function genera_token ($user, $profile)
+{
+	$token = "";
+
+	/* Aqui estaria el algoritmo para la generacion
+	del token */
+
+	return $token;
+}
+
+?>
